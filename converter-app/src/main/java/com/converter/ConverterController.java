4eeb133bc1fd8ff76d707ba14b9a7e0b7d83a91d
d.kumar <d.kumar@10.0.75.1>
2020-12-20 13:05:15 +0530

    Path to query parameter
    Changes

--- converter-app/src/main/java/com/converter/ConverterController.java
+++ converter-app/src/main/java/com/converter/ConverterController.java
@@ -3,6 +3,7 @@ package com.converter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,23 +12,23 @@ public class ConverterController {
 	@Autowired
 	private IConverterService converterService;
 
-	@GetMapping(value = "/conversions/ctok/{value}")
-	public double celciusToKelvin(@PathVariable double value) {
+	@GetMapping(value = "/conversions/ctok")
+	public double celciusToKelvin(@RequestParam double value) {
 		return converterService.celsiusToKelvin(value);
 	}
 
-	@GetMapping(value = "/conversions/ktoc/{value}")
-	public double kelvinToCelcius(@PathVariable double value) {
+	@GetMapping(value = "/conversions/ktoc")
+	public double kelvinToCelcius(@RequestParam double value) {
 		return converterService.kelvinToCelsius(value);
 	}
 
-	@GetMapping(value = "/conversions/ktom/{value}")
-	public double kilometerToMile(@PathVariable double value) {
+	@GetMapping(value = "/conversions/ktom")
+	public double kilometerToMile(@RequestParam double value) {
 		return converterService.kilometerToMile(value);
 	}
 
-	@GetMapping(value = "/conversions/mtok/{value}")
-	public double mileToKilometer(@PathVariable double value) {
+	@GetMapping(value = "/conversions/mtok")
+	public double mileToKilometer(@RequestParam double value) {
 		return converterService.mileToKilometer(value);
 	}
 
